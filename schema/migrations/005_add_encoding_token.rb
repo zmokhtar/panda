5df700061847df35de7354b77ada1952c8253998
Martyn Loughran <martyn@new-bamboo.co.uk>
2008-10-06 14:33:52 +0100

    Removed all the old schema files (relic from the past)

--- schema/migrations/005_add_encoding_token.rb
+++
@@ -1,8 +0,0 @@
-class AddEncodingToken < ActiveRecord::Migration
-  def self.up
-    add_column :encodings, :token, :string
-  end
-
-  def self.down
-  end
-end
--- schema/migrations/006_rename_formats.rb
+++
@@ -1,8 +0,0 @@
-class RenameFormats < ActiveRecord::Migration
-  def self.up
-    rename_column :formats, :format, :code
-  end
-
-  def self.down
-  end
-end
