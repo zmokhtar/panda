10dbe439374abbc311fa879a69a526bbe7a4c176
Martyn Loughran <martyn@new-bamboo.co.uk>
2008-10-20 19:56:44 +0100

    Update merb app for 0.9.8

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,1 +1,8 @@
-puts "Loaded TEST Environment..."
+Merb.logger.info("Loaded TEST Environment...")
+Merb::Config.use { |c|
+  c[:testing] = true
+  c[:exception_details] = true
+  c[:log_auto_flush ] = true
+  c[:log_level] = :fatal
+  c[:log_file] = Merb.log_path / 'merb_test.log'
+}
