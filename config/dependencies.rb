24bc5fc9a534b53fd45a78a5a98b821e1d6e39c0
Martyn Loughran <martyn@new-bamboo.co.uk>
2008-10-22 13:38:51 +0100

    Freeze merb 0.9.10 (AKA RC2) and all required gems except:
    * rvideo (needs custom install)
    * simpledb related - still in flux

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,6 +1,6 @@
 # dependencies are generated using a strict version, don't forget to edit the dependency versions when upgrading.
 merb_gems_version = "0.9.10"
-dm_gems_version   = "0.9.7"
+dm_gems_version   = "0.9.6"
 
 # For more information about each component, please read http://wiki.merbivore.com/faqs/merb_components
 dependency "merb-assets", merb_gems_version  
@@ -8,12 +8,13 @@ dependency "merb-helpers", merb_gems_version
 dependency "merb-mailer", merb_gems_version  
  
 dependency "dm-core", dm_gems_version         
+dependency 'dm-timestamps', dm_gems_version
+dependency "merb_datamapper", merb_gems_version
 
-dependency 'uuid'
+dependency 'uuid', '2.0.1'
 dependency 'amazon_sdb'
-dependency 'activesupport'
+dependency 'activesupport', '2.1.1'
 dependency 'rvideo'
-dependency 'dm-timestamps'
 
 # Dependencies in lib - not autoloaded in time so require them explicitly
 require 'simple_db'
