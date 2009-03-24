dd343b123c58d0535e05d528090cbbfc56cdae33
Damien Tanner <damien@new-bamboo.co.uk>
2009-03-24 01:01:12 +0000

    change log level of encoder and notifier to warn and enable log_auto_flush

--- config/environments/encoder.rb
+++ config/environments/encoder.rb
@@ -2,7 +2,8 @@ Merb.logger.info("Loaded ENCODER Environment...")
 Merb::Config.use { |c|
   c[:exception_details] = false
   c[:reload_classes] = false
-  c[:log_level] = :error
+  c[:log_auto_flush ] = true
+  c[:log_level] = :warn
   
   c[:log_file]  = Merb.root / "log" / "encoder.log"
   # or redirect logger using IO handle
--- config/environments/notifier.rb
+++ config/environments/notifier.rb
@@ -2,7 +2,8 @@ Merb.logger.info("Loaded NOTIFIER Environment...")
 Merb::Config.use { |c|
   c[:exception_details] = false
   c[:reload_classes] = false
-  c[:log_level] = :error
+  c[:log_auto_flush ] = true
+  c[:log_level] = :warn
   
   c[:log_file]  = Merb.root / "log" / "notifier.log"
   # or redirect logger using IO handle
